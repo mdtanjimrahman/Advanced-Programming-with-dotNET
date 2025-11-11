49e92b609df25c9dda4b103461b00e663a5bac4a
mdtanjimrahman <tanjimpromise252000@gmail.com>
2025-11-11 12:57:13 +0600

    folder path restructured

---
+++ first_project/Models/Reference.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace first_project.Models
+{
+    public class Reference
+    {
+        public string[] Name { get; set; }
+        public string[] Status { get; set; }
+    }
+}
