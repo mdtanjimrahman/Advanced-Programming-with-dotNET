781c9060536f7c3f1fb76ab73532f467d048df20
mdtanjimrahman <tanjimpromise252000@gmail.com>
2025-12-16 22:16:16 +0600

    API service established

---
+++ IntroWebAPICore/IntroWebAPICore/Controllers/DepartmentController.cs
@@ -0,0 +1,56 @@
+﻿using IntroWebAPICore.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntroWebAPICore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult All()
+        {
+            var d1 = new DepartmentDTO()
+            {
+                Id = 1,
+                Name = "D1"
+            };
+            var d2 = new DepartmentDTO()
+            {
+                Id = 2,
+                Name = "D2"
+            };
+            var dept = new List<DepartmentDTO>() { d1, d2 };
+            return Ok(dept);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var d1 = new DepartmentDTO()
+            {
+                Id = id,
+                Name = "D" + id
+            };
+            return Ok(d1);
+        }
+
+        [HttpGet("id/{id}/name/{name}")]
+        public IActionResult Get(int id, string name)
+        {
+            var d1 = new DepartmentDTO()
+            {
+                Id = id,
+                Name = name
+            };
+            return Ok(d1);
+        }
+
+        [HttpPost]
+        public IActionResult Create(DepartmentDTO d)
+        {
+            return Ok(d);
+        }
+    }
+}
