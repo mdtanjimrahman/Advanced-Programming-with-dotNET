a2a6a11035ffa2e3fc899af9cc29f954868603e8
mdtanjimrahman <tanjimpromise252000@gmail.com>
2025-12-30 21:07:27 +0600

    initial commit

---
+++ AppTierCore/DAL/EF/CoreUMSContext.cs
@@ -0,0 +1,15 @@
+﻿using DAL.EF.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.EF
+{
+    public class CoreUMSContext : DbContext
+    {
+        public CoreUMSContext(DbContextOptions<CoreUMSContext>options)
+        : base(options) { }
+        public DbSet<Student> Students { get; set; }
+    }
+}
