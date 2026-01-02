644a95aa7f5eea7907fb36a05e623094734a0ee6
mdtanjimrahman <tanjimpromise252000@gmail.com>
2026-01-02 21:10:28 +0600

    error fixed

---
+++ TierArchDemo/DAL/EF/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DAL.EF.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]  
+        [Column(TypeName = "varchar")]
+        public string Name { get; set; }
+        
+        [Required]
+        public double Cgpa { get; set; }
+    }
+}
---
+++ TierArchDemo/DAL/EF/TierArchContext.cs
@@ -0,0 +1,16 @@
+﻿using DAL.EF.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.EF
+{
+    public class TierArchContext : DbContext
+    {
+        public TierArchContext(DbContextOptions<TierArchContext> options)
+        : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
